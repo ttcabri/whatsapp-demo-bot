3e71b5f30dbb348e6ef1a604d81dfe3e37d50164
Andrii Kovalov <ttcabri@gmail.com>
2026-06-15 22:51:19 +0200

    Update services.py

--- handlers/services.py
+++ handlers/services.py
@@ -1,12 +1,12 @@
 from data.business_info import SERVICES_TEXT, PRICES_TEXT
 from services.whatsapp import send_message
 
+BACK_TO_MENU = "\n\n_Reply *Book Now* to book an appointment or *menu* to go back._"
+
 
 def send_services(to: str) -> None:
-    """Send the list of services."""
-    send_message(to, SERVICES_TEXT)
+    send_message(to, SERVICES_TEXT + BACK_TO_MENU)
 
 
 def send_prices(to: str) -> None:
-    """Send the price list."""
-    send_message(to, PRICES_TEXT)
+    send_message(to, PRICES_TEXT + BACK_TO_MENU)
